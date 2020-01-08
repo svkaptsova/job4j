64629460f93d4a0236e2176e0ef48ba1c8a45458
svkaptsova <svkapcova@gmail.com>
2020-01-09 00:01:57 +0300

    5.7. Ипотека. [#156315]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            amount = (amount + amount * percent * 0.01) - salary;
+            year += 1;
+        }
+        return year;
+    }
+}
+
+
+
+
+
