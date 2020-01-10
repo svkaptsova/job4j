8aa6fc86c73142aa1c83e59aa78cc4dcc73cc666
svkaptsova <svkapcova@gmail.com>
2020-01-10 19:20:26 +0300

    5.8. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,61 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator Для произведения арифметических опреаций +-/*.
+ * @author Svetlana Kaptsova
+ * @since 10.01.2020
+ * @version 1.1
+ */
 public class Calculator {
+    /**
+     * Сложение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * result сумма
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + "+" + second + "=" + result);
     }
+    /**
+     * Деление
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * result частное
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + "/" + second + "=" + result);
     }
+    /**
+     * Умножение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * result произведение
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + "*" + second + "=" + result);
     }
+    /**
+     * Вычитание
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * result разность
+     */
     public static void subtract(double first, double second) {
         double result = first - second;
         System.out.println(first + "-" + second + "=" + result);
     }
+
+    /**
+     * Main Точка входа в программу.
+     * @param args - args
+     * add складывает
+     * div делит
+     * multiply умножает
+     * subtract вычитает
+     */
     public static void main(String[] args) {
         add(1,1);
         add(2,2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Package for calculate task.
+ * @see ru.job4j.calculator.Calculator - выполняет арифметические операции
+ * @see ru.job4j.calculator.Fit - вычисляет оптимальный вес
+ * @see ru.job4j.calculator.Git - просто класс для тренировки
+ *
+ * @author Svetlana Kaptsova
+ * @since 10.01.2020
+ * @version 0.1
+ */
+package ru.job4j.calculator;
