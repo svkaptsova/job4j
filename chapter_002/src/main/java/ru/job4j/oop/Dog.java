ad9ee49e66da42d1ad89c1b411192b466fa4e5af
svkaptsova <svkapcova@gmail.com>
2020-02-14 17:44:34 +0300

    1.2. Вызов метода объекта [#166085]

--- chapter_002/src/main/java/ru/job4j/oop/Dog.java
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -1,5 +1,12 @@
 package ru.job4j.oop;
 
+/**
+ * Dog Пробный класс для тренировки создания объектов.
+ *
+ *  @author Svetlana Kaptsova
+ *  @version 1.0
+ *  @since 14.02.2020
+ */
 public class Dog {
     public static void main(String[] args) {
         Dog polkan = new Dog();
