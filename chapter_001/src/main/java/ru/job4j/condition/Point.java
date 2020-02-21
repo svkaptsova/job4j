504fbc88f7759da4beee9787ec8cbac5a4898a73
svkaptsova <svkapcova@gmail.com>
2020-02-21 20:57:30 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,28 +1,36 @@
 package ru.job4j.condition;
 
-/**
- * @author Kaptsova Svetlana (kaptsulka@mail.ru)
- * @version 1.0
- * @since 0.2
- */
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
 /**
- * Растояния между двумя точками в системе координат.
- * @return distance between two point.
+ * Point - Класс для вычисления растояния между двумя точками в системе координат.
+ *
+ * @author Svetlana Kaptsova
+ * @version 1.1
+ * @since 21.02.2020
  */
-
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double x3 = x2 - x1;
-        double y3 = y2 - y1;
-        double first = Math.pow(x3, 2);
-        double second = Math.pow(y3, 2);
-        double a = first + second;
-        return Math.sqrt(a);
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    /**
+     * @param that - точка с заданными координатами
+     * @return расстояние между двумя точками
+     */
+    public double distance(Point that) {
+        return sqrt(pow((that.x - this.x), 2) + pow((that.y - this.y), 2));
     }
 
     public static void main(String[] args) {
-        double result = distance(1, 3, 4, 7);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+        Point a = new Point(1, 3);
+        Point b = new Point(4, 7);
+        double result = a.distance(b);
+        System.out.println("result (1, 3) to (4, 7) " + result);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -4,14 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class PointTest {
-        @Test
-        public void whenPutForPoint1CoordinatesX1Y3AndForPoint2CoordinatesX3Y7ThenGet5() {
-            int inx1 = 1;
-            int iny1 = 3;
-            int inx2 = 4;
-            int iny2 = 7;
-            int expected = 5;
-            double out = Point.distance(inx1, iny1, inx2, iny2);
-            Assert.assertEquals(expected, out, 0.01);
-        }
+    @Test
+    public void whenPutForPointACoordinatesX1Y3AndForPointBCoordinatesX3Y7ThenGet5() {
+        Point a = new Point(1, 3);
+        Point b = new Point(4, 7);
+        int expected = 5;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
 }
