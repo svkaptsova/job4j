dcb5a40625bb0a5e96cc5ab1a8eba0eeae051941
svkaptsova <svkapcova@gmail.com>
2020-03-12 14:46:55 +0300

    5. Аннотация @Override [#174891]

--- chapter_002/src/main/java/ru/job4j/oop/overriding/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/oop/overriding/JSONReport.java
@@ -4,10 +4,12 @@ package ru.job4j.oop.overriding;
  * JSONReport - подкласс для изучения механизма переопределения метода.
  *
  * @author Svetlana Kaptsova (svkapcova@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  */
 public class JSONReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return "name : " + name + "\n" + "body : " + body;
     }
