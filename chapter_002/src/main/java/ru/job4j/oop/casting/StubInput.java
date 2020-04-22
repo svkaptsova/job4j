0882354893ad533e898e37ce65f2e0b7e2118901
svkaptsova <svkapcova@gmail.com>
2020-04-22 18:55:08 +0300

    5. Input и полиморфизм. [#181094]

--- chapter_002/src/main/java/ru/job4j/oop/casting/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/oop/casting/StubInput.java
@@ -2,19 +2,26 @@ package ru.job4j.oop.casting;
 
 public class StubInput implements Input {
     /**
-     * Input - класс для примера использования интерфейса
+     * StubInput - симулятор ввода данных пользователем
      *
      * @author Svetlana Kaptsova (svkapcova@gmail.com)
-     * @version 1.0
+     * @version 1.1
      * @since 1.0
      */
+    private String[] answers;
+    private int position = 0;
+
+    public StubInput(String[] answers) {
+        this.answers = answers;
+    }
+
     @Override
     public String askStr(String question) {
-        return null;
+        return answers[position++];
     }
 
     @Override
     public int askInt(String question) {
-        return 0;
+        return Integer.valueOf(askStr(question));
     }
 }
