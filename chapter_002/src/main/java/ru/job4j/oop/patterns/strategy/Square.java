d91af45182c81d8f2cb72ef5de21fba50ecbad5a
svkaptsova <svkapcova@gmail.com>
2020-04-30 00:48:44 +0300

    7. Шаблон проектирования - Стратегия. [#786] #2

--- chapter_002/src/main/java/ru/job4j/oop/patterns/strategy/Square.java
+++ chapter_002/src/main/java/ru/job4j/oop/patterns/strategy/Square.java
@@ -5,11 +5,11 @@ import java.util.StringJoiner;
 public class Square implements Shape {
     @Override
     public String draw() {
-        StringJoiner pic = new StringJoiner(System.lineSeparator());
-        pic.add(".  .  .  .");
-        pic.add(".        .");
-        pic.add(".        .");
-        pic.add(".  .  .  .");
-        return pic.toString();
+        StringJoiner picture = new StringJoiner(System.lineSeparator());
+        picture.add(".  .  .  .");
+        picture.add(".        .");
+        picture.add(".        .");
+        picture.add(".  .  .  .");
+        return picture.toString();
     }
 }
