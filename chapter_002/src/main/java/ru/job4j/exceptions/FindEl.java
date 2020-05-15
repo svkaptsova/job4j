3c07609404534d70898b053727d3a64f7a50440a
svkaptsova <svkapcova@gmail.com>
2020-05-15 14:38:31 +0300

    0.4. Пользовательские исключения. [#219367] #1

--- chapter_002/src/main/java/ru/job4j/exceptions/FindEl.java
+++ chapter_002/src/main/java/ru/job4j/exceptions/FindEl.java
@@ -2,24 +2,29 @@ package ru.job4j.exceptions;
 
 public class FindEl {
     public static int indexOf(String[] arr, String foo) throws ElementNotFoundException {
-        if (rsl == -1) {
-            throw new ElementNotFoundException("Url could not be null");
-        }
         int rsl = -1;
         for (int index = 0; index < arr.length; index++) {
-            if (arr[index] == foo) {
+            if (arr[index].equals(foo)) {
                 rsl = index;
                 break;
             }
-            return rsl;
         }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
     }
 
     public static void main(String[] args) {
         try {
             indexOf(new String[]{"a", "b", "c"}, "b");
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
+        } catch (ElementNotFoundException el) {
+            el.printStackTrace();
+        }
+        try {
+            indexOf(new String[]{"a", "b", "c"}, "f");
+        } catch (ElementNotFoundException el) {
+            el.printStackTrace();
         }
     }
 }
