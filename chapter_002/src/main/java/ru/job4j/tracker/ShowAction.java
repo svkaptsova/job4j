2997421bf56b802b339e8e7f99aaa822e9399be2
svkaptsova <svkapcova@gmail.com>
2020-07-27 20:17:58 +0300

    6. Изменить программу Tracker из 2-го модуля [#10039]

--- chapter_002/src/main/java/ru/job4j/tracker/ShowAction.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ShowAction.java
@@ -1,10 +1,12 @@
 package ru.job4j.tracker;
 
+import java.util.List;
+
 /**
  * ShowAction - класс для просмотра всех заявок
  *
  * @author Svetlana Kaptsova (svkapcova@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  */
 public class ShowAction implements UserAction {
@@ -15,9 +17,9 @@ public class ShowAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        Item[] items = tracker.findAll();
-        for (int i = 0; i < items.length; i++) {
-            System.out.print(items[i]);
+        List<Item> items = tracker.findAll();
+        for (int i = 0; i < items.size(); i++) {
+            System.out.print(items.get(i));
             System.out.println();
         }
         System.out.println();
