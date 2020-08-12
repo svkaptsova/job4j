f82cbe1b15b37d82d509ee250482cc63e0d07b43
svkaptsova <svkapcova@gmail.com>
2020-08-12 20:40:37 +0300

    2. Преобразования List в Map. [#10093] #1

--- chapter_002/src/main/java/ru/job4j/collection/OrderConvert.java
+++ chapter_002/src/main/java/ru/job4j/collection/OrderConvert.java
@@ -2,16 +2,17 @@ package ru.job4j.collection;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * OrderConvert - класс для преобразования List в Map
  *
  * @author Svetlana Kaptsova (svkapcova@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  */
 public class OrderConvert {
-    public static HashMap<String, Order> process(List<Order> orders) {
+    public static Map<String, Order> process(List<Order> orders) {
         HashMap<String, Order> map = new HashMap<>();
         for (Order order : orders) {
             map.put(order.getNumber(), order);
--- chapter_002/src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -3,8 +3,8 @@ package ru.job4j.collection;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -14,7 +14,7 @@ public class OrderConvertTest {
     public void whenSingleOrder() {
         List<Order> orders = new ArrayList<>();
         orders.add(new Order("3sfe", "Dress"));
-        HashMap<String, Order> map = OrderConvert.process(orders);
+        Map<String, Order> map = OrderConvert.process(orders);
         assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
     }
 }
