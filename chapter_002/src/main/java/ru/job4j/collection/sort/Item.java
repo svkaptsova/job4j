398b1166a95cb1550a83c00fa72ce1091f3e4ae5
svkaptsova <svkapcova@gmail.com>
2020-08-29 18:15:23 +0300

    0. Сортировка [#10096] #0

---
+++ chapter_002/src/main/java/ru/job4j/collection/sort/Item.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection.sort;
+
+/**
+ * Item - модель заявки
+ *
+ * @author Svetlana Kaptsova (svkapcova@gmail.com)
+ * @version 1.0
+ * @since 1.0
+ */
+public class Item implements Comparable<Item> {
+    private int id;
+    private String name;
+
+    public Item(int id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " " + this.name;
+    }
+
+    @Override
+    public int compareTo(Item item) {
+        return Integer.compare(id, item.id);
+    }
+}
