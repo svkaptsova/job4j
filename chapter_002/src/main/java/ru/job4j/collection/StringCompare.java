9a0c2aa16da3baf26c0babe660051029ad4628ac
svkaptsova <svkapcova@gmail.com>
2020-09-20 20:39:05 +0300

    3. Компаратор для строк [#35008] №1

--- chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,25 +5,19 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int resalt = 0;
-        if (o1.toCharArray().length < o2.toCharArray().length) {
-            for (int i = 0; i <= o1.toCharArray().length; i++) {
+        int result = 0;
+        int size = Math.min (o1.toCharArray().length, o2.toCharArray().length);
+
+            for (int i = 0; i <size; i++) {
                 int rsl = Character.compare(o1.toCharArray()[i], o2.toCharArray()[i]);
                 if (rsl != 0) {
-                    resalt = rsl;
+                    result = rsl;
                     break;
                 }
             }
-        }
-        if (o2.toCharArray().length < o1.toCharArray().length) {
-            for (int i = 0; i <= o2.toCharArray().length; i++) {
-                int rsl = Character.compare(o1.toCharArray()[i], o2.toCharArray()[i]);
-                if (rsl != 0) {
-                    resalt = rsl;
-                    break;
-                }
+            if  (result == 0) {
+                result = Integer.compare(o1.toCharArray().length, o2.toCharArray().length);
             }
-        }
-        return resalt;
+        return result;
     }
 }
