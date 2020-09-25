93fb42a26a41e05de458375977ce3a886f12a70e
svkaptsova <svkapcova@gmail.com>
2020-09-25 22:58:57 +0300

    Доп. задание - треугольник #1

--- chapter_001/src/main/java/ru/job4j/array/TrnglMatrix.java
+++ chapter_001/src/main/java/ru/job4j/array/TrnglMatrix.java
@@ -3,16 +3,18 @@ package ru.job4j.array;
 import java.util.Arrays;
 
 class TrnglMatrix {
-    public static void rows(int count) {
-       int a = 1;
-        for (int i = 0; i < count-1; i++) {
-            int[] trngl = new int[i+1];
+    public static int[][] rows(int count) {
+        int[][] triangle = new int[count][];
+        int a = 1;
+        for (int i = 0; i < count - 1; i++) {
+            int[] trngl = new int[i + 1];
             for (int j = 0; j < i + 1; j++) {
                 trngl[j] = a;
                 a++;
             }
             System.out.println(Arrays.toString(trngl));
         }
+        return triangle;
     }
 
     public static void main(String[] args) {
