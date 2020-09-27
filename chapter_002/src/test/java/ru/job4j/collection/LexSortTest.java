2c1435fce49c495d9732b90e2d3979b4e44cd29f
svkaptsova <svkapcova@gmail.com>
2020-09-27 21:01:49 +0300

    4. Сортировка номера [#221284] #0

--- chapter_002/src/test/java/ru/job4j/collection/LexSortTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/LexSortTest.java
@@ -1,7 +1,10 @@
 package ru.job4j.collection;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
+
 import org.junit.Test;
+
 import java.util.Arrays;
 
 public class LexSortTest {
