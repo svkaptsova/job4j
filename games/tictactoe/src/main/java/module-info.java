1f7f06a5ce7e269cabfcf8a0b6652d95e2f49348
svkaptsova <svkapcova@gmail.com>
2020-09-30 21:59:59 +0300

    2. Travis CI #31727

--- games/tictactoe/src/main/java/module-info.java
+++ games/tictactoe/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module tictactoe {
     requires javafx.fxml;
     requires javafx.controls;
-    opens ru.tic to javafx.fxml;
-    exports ru.tic;
+    opens ru.job4j.tictactoe to javafx.fxml;
+    exports ru.job4j.tictactoe;
 }
