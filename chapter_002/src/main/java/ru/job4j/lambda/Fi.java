29cccbba475f7f47e175cfcb964734627be33e79
svkaptsova <svkapcova@gmail.com>
2020-10-24 23:31:22 +0300

    2. Функциональный интерфейс [#247497]

---
+++ chapter_002/src/main/java/ru/job4j/lambda/Fi.java
@@ -0,0 +1,37 @@
+package ru.job4j.lambda;
+
+import ru.job4j.tracker.StubInput;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+public class Fi {
+    public static void main(String[] args) {
+        List<Student> std = Arrays.asList(
+                new Student(new Mark("mark 1"), "First", 15),
+                new Student(new Mark("mark 2"), "Second", 20),
+                new Student(new Mark("mark 3"), "Third", 30)
+                );
+                Comparator<Student> comparator = new Comparator<Student>() {
+                    @Override
+                    public int compare(Student st1, Student st2) {
+                        return st2.getAge() - st1.getAge();
+                    }
+                };
+        std.sort(comparator);
+    }
+    Function<Student, Mark> func = new Function<Student, Mark>() {
+        @Override
+       public Mark apply(Student s) {
+            return new Mark(s.getName());
+        }
+    };
+    public static void raw(List<Student> list, Function<Student, Mark> func) {
+        for (Student st : list) {
+            func.apply(st);
+        }
+    }
+}
+
---
+++ chapter_002/src/main/java/ru/job4j/lambda/Mark.java
@@ -0,0 +1,12 @@
+package ru.job4j.lambda;
+
+public class Mark {
+
+        String name;
+        Mark(String name) {
+            this.name = name;
+        }
+        String getName() {
+            return name;
+        }
+}
