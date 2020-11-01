7c5225d3767a626057439b5bd8bb9a98f3798c72
svkaptsova <svkapcova@gmail.com>
2020-11-01 20:14:46 +0300

    6. Функции высшего порядка [#24255] #1

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -7,7 +7,7 @@ import java.util.function.Predicate;
  * PhoneDictionary - телефонный справочник
  *
  * @author Svetlana Kaptsova (svkapcova@gmail.com)
- * @version 1.1
+ * @version 1.2
  * @since 1.0
  */
 public class PhoneDictionary {
@@ -24,39 +24,12 @@ public class PhoneDictionary {
      * @return Список подошедших пользователей.
      */
     public ArrayList<Person> find(String key) {
-        Predicate<Person> contName = (str) -> {
-            boolean rsl = false;
-            for (int index = 0; index < persons.size(); index++) {
-                rsl = persons.get(index).getName().contains(key);
-            }
-            return rsl;
-        };
+        Predicate<Person> contName = (person) -> person.getName() != null && person.getName().contains(key);
+        Predicate<Person> contSurname = (person) -> person.getSurname() != null && person.getSurname().contains(key);
+        Predicate<Person> contAddress = (person) -> person.getAddress() != null && person.getAddress().contains(key);
+        Predicate<Person> contPhone = (person) -> person.getPhone() != null && person.getPhone().contains(key);
 
-        Predicate<Person> contSurname = (str) -> {
-            boolean rsl = false;
-            for (int index = 0; index < persons.size(); index++) {
-                rsl = persons.get(index).getSurname().contains(key);
-            }
-            return rsl;
-        };
-
-        Predicate<Person> contAdress = (str) -> {
-            boolean rsl = false;
-            for (int index = 0; index < persons.size(); index++) {
-                rsl = persons.get(index).getAddress().contains(key);
-            }
-            return rsl;
-        };
-
-        Predicate<Person> contPhone = (str) -> {
-            boolean rsl = false;
-            for (int index = 0; index < persons.size(); index++) {
-                rsl = persons.get(index).getPhone().contains(key);
-            }
-            return rsl;
-        };
-
-        Predicate<Person> combine = contName.or(contSurname.or(contPhone.or(contAdress)));
+        Predicate<Person> combine = contName.or(contSurname.or(contPhone.or(contAddress)));
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (combine.test(person)) {
