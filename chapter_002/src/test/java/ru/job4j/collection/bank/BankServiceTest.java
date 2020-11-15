4fbeaed8754d39e019d6385a7489b265b4124981
svkaptsova <svkapcova@gmail.com>
2020-11-15 15:27:15 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260] #0

--- chapter_002/src/test/java/ru/job4j/collection/bank/BankServiceTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/bank/BankServiceTest.java
@@ -3,6 +3,7 @@ package ru.job4j.collection.bank;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
 
 public class BankServiceTest {
