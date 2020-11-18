104923327de31561d1d1c768b1d2b817847aafd9
svkaptsova <svkapcova@gmail.com>
2020-11-18 22:10:27 +0300

    0. Stream API улучшения [#58167] #0

--- chapter_002/src/main/java/ru/job4j/stream/School.java
+++ chapter_002/src/main/java/ru/job4j/stream/School.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
  * в зависимости от общего балла по всем предметам
  *
  * @author Svetlana Kaptsova (svkapcova@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  */
 public class School {
@@ -24,6 +24,21 @@ public class School {
             List<Student> students, Predicate<Student> predicate) {
         return students.stream()
                 .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Метод отбирает учеников с баллом > bound
+     *
+     * @param students  - список всех учеников
+     * @param bound - проходной балл
+     * @return - список группы в соответствии с проходным баллом
+     */
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .filter(st -> st != null)
+                .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
+                .takeWhile(st -> st.getScore() > bound)
                 .collect(Collectors.toList());
     }
 }
