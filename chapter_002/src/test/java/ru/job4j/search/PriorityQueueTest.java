41629150c7ac49f271103d1d1d335b47340324a4
svkaptsova <svkapcova@gmail.com>
2020-11-21 23:14:53 +0300

    2. Local-Variable Type Inference [#70626]

--- chapter_002/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_002/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -1,9 +1,8 @@
 package ru.job4j.search;
 
 import org.junit.Test;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class PriorityQueueTest {
     @Test
